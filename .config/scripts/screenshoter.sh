120e5ee041d222bcdb9bfc18f609a7aa1ce73807
dickus <dickie.dickus@proton.me>
2024-01-01 11:14:20 +0300

    fixes for screenshot tool

---
+++ .config/scripts/screenshoter.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+arg=$1
+f="screen_$(date +%F_%H%M%S)".png
+
+case "$arg" in
+    screen) import -window root $HOME/Pictures/"$f" && xclip -sel clip -t image/png -i $HOME/Pictures/"$f";;
+    window) import -window "$(xdotool getwindowfocus)" $HOME/Pictures/"$f" && xclip -sel clip -t image/png -i $HOME/Pictures/"$f";;
+    region) import $HOME/Pictures/"$f" && xclip -sel clip -t image/png -i $HOME/Pictures/"$f";;
+esac
+
